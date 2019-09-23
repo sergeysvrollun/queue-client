2cb07337818a21f46a7de8b6efafdf60ff8b9ab0
Serhii Svintsitskyi <sergeysvrollun@gmail.com>
2019-09-23 16:46:02 +0300

    create project

---
+++ public_html/read.php
@@ -0,0 +1,16 @@
+<?php
+
+use ReputationVIP\QueueClient\QueueClient;
+use ReputationVIP\QueueClient\Adapter\FileAdapter;
+
+require "bootstrap.php";
+
+
+$adapter = new FileAdapter('/tmp');
+$queueClient = new QueueClient($adapter);
+
+
+$messages = $queueClient->getMessages('testQueue');
+$message = $messages[0];
+$queueClient->deleteMessage($message);
+echo $message['Body'];
---
+++ public_html/write.php
@@ -0,0 +1,12 @@
+<?php
+
+use ReputationVIP\QueueClient\QueueClient;
+use ReputationVIP\QueueClient\Adapter\FileAdapter;
+
+require "bootstrap.php";
+
+
+$adapter = new FileAdapter('/tmp');
+$queueClient = new QueueClient($adapter);
+
+$queueClient->addMessage('testQueue', 'testMessage');
